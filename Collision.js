81b1d1e962d449dc7d1203a79318e6b260255a07
dimplekp <dimplepatel291@gmail.com>
2015-05-08 17:23:56 +0530

    Kill balloons when they go out of the world bounds

--- Collision.js
+++ Collision.js
@@ -31,7 +31,7 @@ function create() {
                                       // behavior of P2 Physics
   balloon1.body.setCircle(45); // Detect collision only on balloon
                                // i.e., on circle with radius 45
-
+  balloon1.outOfBoundsKill = true
   // BALLOON 2
 
   balloon2 = game.add.sprite(300, 600, 'balloon');
@@ -41,6 +41,7 @@ function create() {
   balloon2.body.velocity.y = -200;
   balloon2.body.fixedRotation = true;
   balloon2.body.setCircle(45);
+  balloon2.outOfBoundsKill = true
 
   // BALLOON 3
 
@@ -51,6 +52,7 @@ function create() {
   balloon3.body.velocity.y = -300;
   balloon3.body.fixedRotation = true;
   balloon3.body.setCircle(45);
+  balloon3.outOfBoundsKill = true
 
 }
 
